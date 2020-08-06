c773498f70b61f0b5a546bea61d3a6039362cafa
qiankun <qiankun@taptap.com>
2020-08-06 14:43:24 +0800

    Revert "delete"
    This reverts commit c735c0467a3e92af30e6e2a07192435393c655d0.

---
+++ out/QPosTest.framework/Headers/QPosTest.h
@@ -0,0 +1,19 @@
+//
+//  QPosTest.h
+//  QPosTest
+//
+//  Created by Bottle K on 2020/8/6.
+//  Copyright © 2020 q. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QPosTest.
+FOUNDATION_EXPORT double QPosTestVersionNumber;
+
+//! Project version string for QPosTest.
+FOUNDATION_EXPORT const unsigned char QPosTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QPosTest/PublicHeader.h>
+
+
